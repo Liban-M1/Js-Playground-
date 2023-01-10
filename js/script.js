07da9856cf07ee7d3247648fc144ac78a6dfb100
Liban-M1 <114482781+Liban-M1@users.noreply.github.com>
2023-01-10 04:04:36 +0100

    .

---
+++ js/script.js
@@ -0,0 +1,74 @@
+// Question 1
+var numbreVariable1 = 1;
+var numbreVariable2 = 2.2;
+
+// Question 2
+var workingDays = "Monday - Tuesday - Wednsday - Thursday - Friday";
+
+console.log(workingDays);
+
+// Question 3
+var addition = "1 + 1" ;
+var substraction = "2 - 2";
+var multiplication = "3 * 3";
+var division = "4 / 4";
+
+// Question 4
+var wrong = false;
+
+// Question 5 
+var q5 = "The frogs are angry";
+
+console.log(q5.length);
+
+// Question 6 
+var timeElapsed; 
+var totalPrice; 
+
+
+// Question 7 
+var number1 = "53";
+var number2 = "44.6";
+
+var int1 = parseInt(number1);
+
+var float1 = parseFloat(number2);
+
+console.log(int1);  // Output: 53
+console.log(float1);  // Output: 44.6
+
+// Question 8
+var pet = "donkey";
+var age = 89;
+var discountApplied = true;
+var timeLeft;
+
+var tyeOfPet = typeof pet;
+console.log(typeOfPet)
+
+var typeOfAge = typeof age;
+console.log(typeOfAge);
+
+var typeOfDiscountApplied = typeof discountApplied;
+console.log(typeOfDiscountApplied)
+
+var typeOfTimeLeft = typeof timeLeft;
+console.log(typeOfTimeLeft)
+
+// Question 9 
+var heading = "Updating heading";
+
+var heading = document.querySelector('h1');
+
+heading.innerHTML = headingText;
+
+// Question 10
+var body = document.querySelector("body");
+body.style.backgroundColor = "lightskyblue";
+
+
+
+
+
+
+
