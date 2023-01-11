608dbc0bb4209aac245e27f9ffa1866c87f41b3f
Liban-M1 <114482781+Liban-M1@users.noreply.github.com>
2023-01-11 21:02:53 +0100

    .

---
+++ pre.js
@@ -0,0 +1,174 @@
+Task (1)
+
+/*
+// Question 1
+var numbreVariable1 = 1;
+var numbreVariable2 = 2.2;
+
+// Question 2
+var workingDays = "Monday - Tuesday - Wednsday - Thursday - Friday";
+
+console.log(workingDays);
+
+// Question 3
+var addition = "1 + 1" ;
+var substraction = "2 - 2";
+var multiplication = "3 * 3";
+var division = "4 / 4";
+
+// Question 4
+var wrong = false;
+
+// Question 5 
+var q5 = "The frogs are angry";
+
+console.log(q5.length);
+
+// Question 6 
+var timeElapsed; 
+var totalPrice; 
+
+
+// Question 7 
+var number1 = "53";
+var number2 = "44.6";
+
+var int1 = parseInt(number1);
+
+var float1 = parseFloat(number2);
+
+console.log(int1);  // Output: 53
+console.log(float1);  // Output: 44.6
+
+// Question 8
+var pet = "donkey";
+var age = 89;
+var discountApplied = true;
+var timeLeft;
+
+var tyeOfPet = typeof pet;
+console.log(typeOfPet)
+
+var typeOfAge = typeof age;
+console.log(typeOfAge);
+
+var typeOfDiscountApplied = typeof discountApplied;
+console.log(typeOfDiscountApplied)
+
+var typeOfTimeLeft = typeof timeLeft;
+console.log(typeOfTimeLeft)
+
+// Question 9 
+var heading = "Updating heading";
+
+var heading = document.querySelector('h1');
+
+heading.innerHTML = headingText;
+
+// Question 10
+var body = document.querySelector("body");
+body.style.backgroundColor = "lightskyblue"; 
+*/
+
+
+
+Task (2)
+
+/*
+// Question 1
+var firstNumber = 34;
+var secondNumber = 89;
+
+if (firstNumber < secondNumber) {
+    console.log("The first number is smaller than the second");
+}
+
+// Question 2
+var age = 11;
+var minimumAge = "13";
+
+    if (age <= minimumAge); {
+        console.log("Under minimumage");
+    }
+
+// Question 3
+var income = "11.050";
+var maximumIncome = "13.075";
+
+if (parseFloat(income) <= parseFloat(maximumIncome)) {
+    console.log("maximum income requirement met");
+} else {
+    console.log("Maximum requirement not met");
+}
+
+
+// Question 4
+var colour = "blue";
+
+if (colour === "orange") {
+    console.log("This colour is a bit rubbish");
+}
+
+
+// Question 5
+var invoicePaid = false;
+
+if (invoicePaid === false){
+    console.log ("Not paid")
+}
+
+
+// Question 6
+var selectedNumber = 11;
+var winningNumber = "15";
+
+    if (selectedNumber !== parseInt(winningNumber)) {
+        console.log ("You lose again");
+}
+
+    if (selectedNumber !== parseInt(winningNumber)) {
+    console.log("You lose again");
+}
+
+
+// Question 7
+
+var daysOfTheWeek = 2;
+var dayOfTheWeek;
+
+    switch (daysOfTheWeek) {
+        case 1:
+            dayOfTheWeek = "Monday";
+            break;
+
+       case 2:
+            dayOfTheWeek = "Tusday";
+            break;
+
+       case 3:
+            dayOfTheWeek = "Wednesday";
+            break;
+
+        case 4:
+            dayOfTheWeek = "Thursday";
+            break;
+
+        case 5:
+            dayOfTheWeek = "Friday";
+            break;
+
+        case 6:
+            dayOfTheWeek = "Saturday";
+            break;
+
+        case 7:
+            dayOfTheWeek = "Sunday";
+            break;
+
+        default:
+            dayOfTheWeek = "Invalid day number";
+            break;  
+    }
+
+    console.log(dayOfTheWeek);
+*/
