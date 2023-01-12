25f504b4b04f6d9927d595f5fa2be324920a87b7
Liban-M1 <114482781+Liban-M1@users.noreply.github.com>
2023-01-12 23:08:28 +0100

    .

--- pre.js
+++ pre.js
@@ -172,3 +172,16 @@ var dayOfTheWeek;
 
     console.log(dayOfTheWeek);
 */
+
+task (3)
+/* count down from 10 to 1
+for (var i = 10; i >= 1; i--) {
+    console.log(i);
+}
+
+
+for (var o = 10; o <= 20; o++) {
+    if (o % 2 !== 0)
+    console.log(o);
+}
+*/
