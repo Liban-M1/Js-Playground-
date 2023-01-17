9c18010155b750ddb58286fe70ae7315e7864d0b
Liban-M1 <114482781+Liban-M1@users.noreply.github.com>
2023-01-17 04:40:36 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,53 +1,112 @@
-// Quesiton 1
+// Question 1
+var product = {
+	name: "Chicken feet",
+	price: 149.99,
+};
 
-var winningNumbers = [1, 76, 2, 78, 16, 7];
-
-console.log(winningNumbers[2]);
+console.log(product.price);
 
 // Question 2
+var animals = [
+	{
+		type: "cat",
+		colour: "black",
+	},
+	{
+		type: "elephant",
+		colour: "grey",
+	},
+	{
+		type: "dog",
+		colour: "brown",
+	},
+	{
+		type: "zebra",
+		colour: "black and white",
+	},
+];
 
-var vegetables = ["rice", "potatoes", "porridge", "cake", "spinach"];
 
-vegetables.push("peas")
-console.log(vegetables.length);
+for (var i = 0; i < animals.length; i++) {
+    var animalsColour = animals[i].colour;
+    console.log(animalsColour);
+}
 
 // Question 3
+var dinnerOptions = [
+	{ name: "Fish and Dips", price: 11.95, onSpecial: true },
+	{ name: "Bacon and Frogs", price: 12.99, onSpecial: false },
+	{ name: "Mince Flies", price: 19.0, onSpecial: true },
+];
 
-var randomThings = ["pumpkin", false, 23, true, "tomato"];
-
-for (var i = 0; i < randomThings.length; i++) {
-    var things = randomThings[i]
-    console.log(things);
+for (var i = 0; i < dinnerOptions.length; i++) {
+    var isOnSpecial = dinnerOptions[i].onSpecial;
+    var productName = dinnerOptions[i].name;
+    
+    if (isOnSpecial === true) {
+        console.log(productName);
+    }
 }
 
 // Question 4
+var people = [
+	{
+		firstName: "Burt",
+		lastName: "Smacharach",
+		age: 35,
+	},
+	{
+		firstName: "Elvis",
+		lastName: "Deadly",
+		age: 55,
+	},
+	{
+		firstName: "Susan",
+		lastName: "Sarongson",
+		age: 42,
+	},
+	{
+		firstName: "Big",
+		lastName: "Gabe",
+		age: 20,
+	},
+];
 
-var moreThings = [14, "cat", true, 29, "fox", true, 34, 19];
+for (var i = 0; i < people.length; i++) {
+    var firstLast = people[i].firstName + " " + people[i].lastName;
+    var ofAge = people[i].age;
 
-var numberValues = moreThings.filter(element => typeof element === "number");
-
-console.log(numberValues);
-
-// Question 5
-
-var ingredients = ["water", "flour", "toothpaste", "fish lips"];
-
-var ingredients1 = ingredients.sort();
-
-for (var o = 0; o < ingredients1.length; o++) {
-    var ingredients2 = ingredients1[o]
-    console.log(ingredients2);
-}
-
-console.log(ingredients1);
-
-// Question 6
-
-var characters = ["Gob", "Michael", "George Michael", "Lindsay", "Buster", "Maeby", "Tobias", "Lucille"];
-
-for (var p = 0; p < characters.length; p++) {
-
-    if (p % 2 === 1) {
-        console.log(characters[p]);
+    if (ofAge >= 20 && ofAge <= 50) {
+        console.log(firstLast)
     }
+    
+    
 }
+
+// Question 5 
+var products = [
+	{
+		name: "Elephant eggs",
+		price: 45.0,
+	},
+	{
+		name: "Bucket of water",
+		price: 459.99,
+	},
+	{
+		name: "4 cabbages",
+		price: 500.95,
+	},
+];
+
+
+
+for (var i = 0; i < products.length; i++) {
+    var productInfo = products[i].name + " - " + products[i].price;
+    productContainer.innerHTML = productContainer.innerHTML + "<li>" + productInfo + "<li>";
+
+}
+
+console.log(productInfo)
+
+
--- pre.js
+++ pre.js
@@ -257,3 +257,61 @@ else {
 }
 */
 
+Task (5)
+
+/*
+// Quesiton 1
+
+var winningNumbers = [1, 76, 2, 78, 16, 7];
+
+console.log(winningNumbers[2]);
+
+// Question 2
+
+var vegetables = ["rice", "potatoes", "porridge", "cake", "spinach"];
+
+vegetables.push("peas")
+console.log(vegetables.length);
+
+// Question 3
+
+var randomThings = ["pumpkin", false, 23, true, "tomato"];
+
+for (var i = 0; i < randomThings.length; i++) {
+    var things = randomThings[i]
+    console.log(things);
+}
+
+// Question 4
+
+var moreThings = [14, "cat", true, 29, "fox", true, 34, 19];
+
+var numberValues = moreThings.filter(element => typeof element === "number");
+
+console.log(numberValues);
+
+// Question 5
+
+var ingredients = ["water", "flour", "toothpaste", "fish lips"];
+
+var ingredients1 = ingredients.sort();
+
+for (var o = 0; o < ingredients1.length; o++) {
+    var ingredients2 = ingredients1[o]
+    console.log(ingredients2);
+}
+
+console.log(ingredients1);
+
+// Question 6
+
+var characters = ["Gob", "Michael", "George Michael", "Lindsay", "Buster", "Maeby", "Tobias", "Lucille"];
+
+for (var p = 0; p < characters.length; p++) {
+
+    if (p % 2 === 1) {
+        console.log(characters[p]);
+    }
+}
+
+*/
